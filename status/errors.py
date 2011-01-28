75a8fd162e3b9011f6621dae593d1e40b23850a9
Mike Taylor <bear@mozilla.com>
2011-01-28 12:02:03 -0500

    bug 625973 r=bhearsum hg checkout failures should be purple, not red

--- status/errors.py
+++ status/errors.py
@@ -6,6 +6,8 @@ global_errors = ((re.compile("No space left on device"), RETRY),
                  (re.compile("Remote Device Error"), EXCEPTION),
                 )
 hg_errors = ((re.compile("abort: HTTP Error 5\d{2}"), RETRY),
-             (re.compile("abort: .*: no match found!"), RETRY)
+             (re.compile("abort: .*: no match found!"), RETRY),
+             (re.compile("abort: Connection reset by peer"), RETRY),
+             (re.compile("transaction abort!"), RETRY),
             )
 purge_error = ((re.compile("Error: unable to free"), RETRY),)
