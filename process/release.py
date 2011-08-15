8a534cc935e38d1d48f2cb7fc4a3170abc1fb599
Rail Aliev <rail@mozilla.com>
2011-08-15 17:58:20 +0300

    Bug 668657 - Updates builder fails on linux64; backed out revision 018f7a79d057.

--- process/release.py
+++ process/release.py
@@ -1117,7 +1117,7 @@ def generateReleaseBranchObjects(releaseConfig, branchConfig,
 
         builders.append({
             'name': builderPrefix('updates'),
-            'slavenames': branchConfig['platforms']['linux']['slaves'],
+            'slavenames': branchConfig['platforms']['linux']['slaves'] + branchConfig['platforms']['linux64']['slaves'],
             'category': builderPrefix(''),
             'builddir': builderPrefix('updates'),
             'slavebuilddir': reallyShort(builderPrefix('updates')),
