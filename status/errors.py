3db14ac1bf46b69db4a2af98c6b7588134d29418
Rail Aliev <rail@mozilla.com>
2011-09-07 10:28:10 +0300

    Bug 683448 - Permission check and virus scan tests should fail if files pushed to the releases directory. r=bhearsum

--- status/errors.py
+++ status/errors.py
@@ -1,10 +1,11 @@
 import re
 
-from buildbot.status.builder import EXCEPTION, FAILURE, RETRY
+from buildbot.status.builder import EXCEPTION, FAILURE, RETRY, WARNINGS
 
 global_errors = ((re.compile("No space left on device"), RETRY),
                  (re.compile("Remote Device Error"), EXCEPTION),
                  (re.compile("Connection to the other side was lost in a non-clean fashion"), RETRY),
+                 (re.compile("WARN(ING)?:"), WARNINGS),
                 )
 hg_errors = ((re.compile("abort: HTTP Error 5\d{2}"), RETRY),
              (re.compile("abort: .*: no match found!"), RETRY),
