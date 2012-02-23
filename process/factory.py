ed19658bd745d2d2b8a38249774570a9ad1f2f6d
Chris Cooper <ccooper@deadsquid.com>
2012-02-23 17:06:39 -0500

    Backout of b7d733964aa5 for bug 660480

--- process/factory.py
+++ process/factory.py
@@ -50,7 +50,7 @@ reload(release.info)
 reload(release.paths)
 
 from buildbotcustom.status.errors import purge_error, global_errors, \
-  upload_errors, tegra_errors
+  upload_errors
 from buildbotcustom.steps.base import ShellCommand, SetProperty, Mercurial, \
   Trigger, RetryingShellCommand, RetryingSetProperty
 from buildbotcustom.steps.misc import TinderboxShellCommand, SendChangeStep, \
@@ -7056,23 +7056,14 @@ class RemoteUnittestFactory(MozillaTestFactory):
             workdir='build/hostutils',
             name='unpack_hostutils',
         ))
-        self.addStep(SetProperty(
-                name='set_toolsdir',
-                command=['bash', '-c', 'pwd'],
-                property='toolsdir',
-                workdir='/builds/tools',
-        ))
-        self.addStep(RetryingShellCommand(
+        self.addStep(ShellCommand(
             name='cleanup device',
             workdir='.',
             description="Cleanup Device",
             command=['python', '/builds/sut_tools/cleanup.py',
                      WithProperties("%(sut_ip)s"),
                     ],
-            haltOnFailure=True,
-            flunkOnFailure=True,
-            log_eval_func=lambda c,s: regex_log_evaluator(c, s, tegra_errors),
-            )
+            haltOnFailure=True)
         )
         self.addStep(ShellCommand(
             name='install app on device',
@@ -7419,13 +7410,7 @@ class TalosFactory(RequestSortingBuildFactory):
          env=self.env)
         )
         if self.remoteTests:
-            self.addStep(SetProperty(
-                    name='set_toolsdir',
-                    command=['bash', '-c', 'pwd'],
-                    property='toolsdir',
-                    workdir='/builds/tools',
-            ))
-            self.addStep(RetryingShellCommand(
+            self.addStep(ShellCommand(
                 name='cleanup device',
                 workdir=self.workdirBase,
                 description="Cleanup Device",
@@ -7433,10 +7418,8 @@ class TalosFactory(RequestSortingBuildFactory):
                          WithProperties("%(sut_ip)s"),
                         ],
                 env=self.env,
-                haltOnFailure=True,
-                flunkOnFailure=True,
-                log_eval_func=lambda c,s: regex_log_evaluator(c, s, tegra_errors),
-            ))
+                haltOnFailure=True)
+            )
         if not self.remoteTests:
             self.addStep(DownloadFile(
              url=WithProperties("%s/tools/buildfarm/maintenance/count_and_reboot.py" % self.supportUrlBase),
