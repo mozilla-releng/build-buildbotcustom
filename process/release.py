febb823361d2cb8912c7e58bc127ebbd52627537
Rail Aliiev <rail@mozilla.com>
2012-03-01 06:43:01 -0500

    Bug 729918 - Investigate why start_uptake_monitoring builder uses wrong script_repo_revision property. r=catlee

--- process/release.py
+++ process/release.py
@@ -1097,7 +1097,10 @@ def generateReleaseBranchObjects(releaseConfig, branchConfig,
             schedulerNames.append(builderPrefix('ready-for-rel-test'))
         trigger_uptake_factory.addStep(Trigger(
             schedulerNames=schedulerNames,
-            copy_properties=['script_repo_revision', 'release_config']
+            set_properties={
+                'release_config': releaseConfigFile,
+                'script_repo_revision': releaseTag,
+            },
         ))
         builders.append({
             'name': builderPrefix('start_uptake_monitoring'),
