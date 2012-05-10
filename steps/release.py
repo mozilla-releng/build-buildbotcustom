d65808387291367e580087ad32da6516147ef8bc
Ben Hearsum <bhearsum@mozilla.com>
2012-05-11 02:09:18 +0800

    bug 749748: kill l10n verify. r=rail

--- steps/release.py
+++ steps/release.py
@@ -18,65 +18,6 @@ class UpdateVerify(ShellCommand):
                 worst = worst_status(worst, FAILURE)
         return worst
 
-class L10nVerifyMetaDiff(TinderboxShellCommand):
-    """Run the l10n verification script.
-    """
-    name='l10n metadiff'
-    description=['create', 'metadiff']
-    descriptionDone=['created', 'metadiff']
-
-    def __init__(self, 
-                 currentProduct=None, 
-                 previousProduct=None,
-                 **kwargs):
-        self.super_class = TinderboxShellCommand
-        kwargs['ignoreCodes'] = [0,1]
-        kwargs['log_eval_func'] = lambda x,y: SUCCESS
-        self.super_class.__init__(self, **kwargs)
-        self.addFactoryArguments(currentProduct=currentProduct,
-                                 previousProduct=previousProduct)
-        if not kwargs.get('command'):
-            if currentProduct is None:
-                return FAILURE
-            if previousProduct is None:
-                return FAILURE
-            self.command=['diff', '-r',
-                          '%s/diffs' % currentProduct,
-                          '%s/diffs' % previousProduct]
-    
-    def evaluateCommand(self, cmd):
-        fileWarnings = self.getProperty('fileWarnings')
-        if fileWarnings and len(fileWarnings) > 0:
-            return WARNINGS
-        '''We ignore failures here on purpose, since diff will 
-           return 1(FAILURE) if it actually finds anything to output.
-        '''
-        return self.super_class.evaluateCommand(self, cmd)
-    
-    def createSummary(self, log):
-        fileWarnings = []
-        unmatchedFiles = []
-        for line in log.readlines():
-            # We want to know about files that are only in one build or the 
-            # other, but we don't consider this worthy of a warning,
-            # e.g. changed search plugins
-            if line.startswith('Only'):
-                unmatchedFiles.append(line)
-                continue
-            # These entries are nice to know about, but aren't fatal. We create
-            # a separate warnings log for them.
-            if line.startswith('> FAIL') or line.startswith('> Binary'):
-                fileWarnings.append(line)
-                continue
-
-        if unmatchedFiles and len(unmatchedFiles) > 0:
-            self.addCompleteLog('Only in...', "".join(unmatchedFiles))
-                              
-        self.setProperty('fileWarnings', fileWarnings)
-        if fileWarnings and len(fileWarnings) > 0:
-            self.addCompleteLog('Warnings', "".join(fileWarnings))
-
-
 
 class SnippetComparison(ShellCommand):
     # Alphas/Betas are WARN'ed about in a releasetest vs release comparison
