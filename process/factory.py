c0e6e9eff5d23b49ce908c520f4890541f7cb4b8
Nick Thomas <nthomas@mozilla.com>
2012-07-30 17:42:33 -0700

    Bug 774199, Sign partial updates in the right order, r=rail

--- process/factory.py
+++ process/factory.py
@@ -2908,7 +2908,7 @@ class ReleaseBuildFactory(MercurialBuildFactory):
         if self.enableSigning and self.signingServers:
             partial_mar_path = '%s/dist/%s/%s' % \
                 (self.absMozillaObjDir, update_dir, partial_mar_name)
-            cmd = '%s -f gpg -f mar "%s"' % (self.signing_command,
+            cmd = '%s -f mar -f gpg "%s"' % (self.signing_command,
                                              partial_mar_path)
             self.addStep(ShellCommand(
                 name='sign_partial_mar',
