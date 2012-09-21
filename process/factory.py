b7096e9de212c509eb55e558abf80f35b6b7e6d0
Rail Aliiev <rail@mozilla.com>
2012-09-21 11:32:21 -0400

    Bug 793088 - Startup failure in localization builds since Aurora17.0a2 20120920042010. r=catlee

--- process/factory.py
+++ process/factory.py
@@ -3484,8 +3484,8 @@ class BaseRepackFactory(MozillaBuildFactory):
         self.addStep(MockCommand(
          name='repack_installers_pretty',
          description=['repack', 'installers', 'pretty'],
-         command=['sh', '-c', 'make', WithProperties('installers-%(locale)s'),
-                  'LOCALE_MERGEDIR=$PWD/merged'],
+         command=['sh', '-c',
+            WithProperties('make installers-%(locale)s LOCALE_MERGEDIR=$PWD/merged')],
          env=prettyEnv,
          haltOnFailure=False,
          flunkOnFailure=False,
@@ -3756,8 +3756,8 @@ class NightlyRepackFactory(BaseRepackFactory, NightlyBuildFactory):
         self.addStep(MockCommand(
          name='repack_installers',
          description=['repack', 'installers'],
-         command=['make', WithProperties('installers-%(locale)s'),
-                  'LOCALE_MERGEDIR=$PWD/merged'],
+         command=['sh', '-c',
+            WithProperties('make installers-%(locale)s LOCALE_MERGEDIR=$PWD/merged')],
          env = self.env,
          haltOnFailure=True,
          workdir='%s/%s/%s/locales' % (self.baseWorkDir, self.objdir, self.appName),
