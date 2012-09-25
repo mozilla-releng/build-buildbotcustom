af30f8e6603e8a43c7fbcb9986c4ad6927c528b2
Aki Sasaki <asasaki@mozilla.com>
2012-09-25 09:37:40 -0700

    bug 793017 - mozharness desktop debug unittests. r=armenzg

--- misc.py
+++ misc.py
@@ -433,7 +433,7 @@ def generateTestBuilder(config, branch_name, platform, name_prefix,
             scriptName=suites['script_path'],
             hg_bin=suites['hg_bin'],
             extra_args=suites.get('extra_args', []),
-            script_maxtime=suites.get('script_maxtime', 1200),
+            script_maxtime=suites.get('script_maxtime', 3600),
             reboot_command=suites.get('reboot_command'),
             platform=platform,
             log_eval_func=lambda c,s: regex_log_evaluator(c, s, (
@@ -1954,7 +1954,7 @@ def generateTalosBranchObjects(branch, branch_config, PLATFORMS, SUITES,
                             }
                             if isinstance(suites, dict) and "mozharness_repo" in suites:
                                 test_builder_kwargs['mozharness'] = True
-                                test_builder_kwargs['mozharness_python'] = platform_config['mozharness_python']
+                                test_builder_kwargs['mozharness_python'] = platform_config['mozharness_config']['mozharness_python']
                             branchObjects['builders'].extend(generateTestBuilder(**test_builder_kwargs))
                             if create_pgo_builders and test_type == 'opt':
                                 pgo_builder_kwargs = test_builder_kwargs.copy()
