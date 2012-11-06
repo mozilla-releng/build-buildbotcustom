ab1f4945eddb8f324668e44713fa1ce00f407559
Justin Wood <Callek@gmail.com>
2012-11-06 17:58:37 -0500

    Bug 808951 - Panda (Android 4.0) jobs for reftest and jsreftest pending forever. r=aki

--- misc.py
+++ misc.py
@@ -397,17 +397,22 @@ def _nextSlowIdleSlave(nReserved):
 # Purge with fire when this is no longer needed
 def _nextOldTegra(builder, available_slaves):
     try:
-        old = []
+        valid = []
         for s in available_slaves:
+            if 'panda-' in s.slave.slavename:
+                # excempt Panda's from this foolishness
+                valid.append(s)
+                continue
+            
             number = s.slave.slavename.replace('tegra-', '')
             try:
                 if int(number) < 286:
-                    old.append(s)
+                    valid.append(s)
             except ValueError:
                 log.msg("Error parsing number out of '%s', discarding from old list" % s.slave.slavename)
                 continue
-        if old:
-            return random.choice(old)
+        if valid:
+            return random.choice(valid)
         return None
     except:
         log.msg("Error choosing old tegra for builder '%s', choosing randomly instead" % builder.name)
