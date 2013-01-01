26a20d9beeb3b6cfff81d470317952e63effc799
Justin Wood <Callek@gmail.com>
2013-01-01 17:49:18 -0500

    Backout Bug 691177 for suspicion of breaking the build

--- misc.py
+++ misc.py
@@ -673,8 +673,7 @@ def generateBranchObjects(config, name, secrets=None):
     weeklyBuilders = []
     coverageBuilders = []
     # prettyNames is a mapping to pass to the try_parser for validation
-    PRETTY_NAME = '%(basename)s %(trystatus)sbuild'
-    NAME = '%(basename)s build'
+    PRETTY_NAME = '%s build'
     prettyNames = {}
     unittestPrettyNames = {}
     unittestSuites = []
@@ -703,6 +702,7 @@ def generateBranchObjects(config, name, secrets=None):
     for platform in enabled_platforms:
         pf = config['platforms'][platform]
         base_name = pf['base_name']
+        pretty_name = PRETTY_NAME % base_name
 
         # short-circuit the extra logic below for debug, l10n, pgo and nightly
         # builds these aren't required (yet) for mozharness builds
@@ -718,15 +718,9 @@ def generateBranchObjects(config, name, secrets=None):
                 nightlyBuilders.append(buildername)
             continue
 
-        values = { 'basename': base_name,
-                   'trystatus': '' if pf.get('try-by-default', True) else 'try-nondefault ',
-                   }
-        pretty_name = PRETTY_NAME % values
-        name = NAME % values
-
         if platform.endswith("-debug"):
-            builders.append(name)
-            buildersByProduct.setdefault(pf['stage_product'], []).append(name)
+            builders.append(pretty_name)
+            buildersByProduct.setdefault(pf['stage_product'], []).append(pretty_name)
             prettyNames[platform] = pretty_name
             # Debug unittests
             if pf.get('enable_unittests'):
@@ -744,8 +738,8 @@ def generateBranchObjects(config, name, secrets=None):
             if not pf.has_key('enable_nightly'):
                 continue
         elif pf.get('enable_dep', True):
-            builders.append(name)
-            buildersByProduct.setdefault(pf['stage_product'], []).append(name)
+            builders.append(pretty_name)
+            buildersByProduct.setdefault(pf['stage_product'], []).append(pretty_name)
             prettyNames[platform] = pretty_name
 
         # Fill the l10n dep dict
@@ -1912,22 +1906,13 @@ def generateTalosBranchObjects(branch, branch_config, PLATFORMS, SUITES,
             talos_builders = {}
             talos_pgo_builders = {}
 
-            try_default = True
-            if not branch_config['platforms'][platform].get('try-by-default', True):
-                try_default = False
-            elif not platform_config.get('try-by-default', True):
-                try_default = False
-
             for slave_platform in slave_platforms:
                 platform_name = platform_config[slave_platform]['name']
                 # this is to handle how a platform has more than one slave platform
-                slave_platform_try_default = try_default
-                if not platform_config[slave_platform].get('try-by-default', True):
-                    slave_platform_try_default = False
-                platformPrettyName = platform_name
-                if not slave_platform_try_default:
-                    platformPrettyName += ' try-nondefault'
-                prettyNames.setdefault(platform, []).append(platformPrettyName)
+                if prettyNames.has_key(platform):
+                    prettyNames[platform].append(platform_name)
+                else:
+                    prettyNames[platform] = [platform_name]
                 for suite, talosConfig in SUITES.items():
                     tests, merge, extra, platforms = branch_config['%s_tests' % suite]
                     if tests == 0 or slave_platform not in platforms:
@@ -2506,8 +2491,6 @@ def generateSpiderMonkeyObjects(project, config, SLAVES):
 
             prettyName = PRETTY_NAME % (base_name, project, variant)
             name = prettyName
-            if not config.get('try-by-default', True):
-                prettyName += ' try-nondefault'
             prettyNames[platform] = prettyName
 
             builder = {'name': name,
