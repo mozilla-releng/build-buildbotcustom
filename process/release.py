0716a5f3471f9df2a7d84c6be80b400f8d690842
Aki Sasaki <asasaki@mozilla.com>
2013-01-17 16:28:02 -0500

    bug 705807 - android signing-on-demand. r=catlee

--- process/release.py
+++ process/release.py
@@ -792,8 +792,9 @@ def generateReleaseBranchObjects(releaseConfig, branchConfig,
 
             for n, builderName in l10nBuilders(platform).iteritems():
                 if hasPlatformSubstring(platform, 'android'):
-                    extra_args = releaseConfig['single_locale_options'][platform] + ['--total-chunks', str(l10nChunks), '--this-chunk', str(n)]
-                    repack_factory = ScriptFactory(
+                    extra_args = releaseConfig['single_locale_options'][platform] + ['--total-chunks', str(l10nChunks), '--this-chunk', str(n), '--key_alias', 'release']
+                    repack_factory = SigningScriptFactory(
+                        signingServers=getSigningServers(platform),
                         scriptRepo=mozharness_repo,
                         scriptName='scripts/mobile_l10n.py',
                         extra_args=extra_args,
