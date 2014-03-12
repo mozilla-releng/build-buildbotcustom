0d7d62e51fb91bc7e4651698092a2f9eaec43a33
Chris AtLee <catlee@mozilla.com>
2014-03-12 17:26:05 -0400

    Bug 976415: get instance metadata from slaves r=bhearsum

--- process/factory.py
+++ process/factory.py
@@ -15,6 +15,7 @@ from buildbot.steps.transfer import FileDownload, JSONPropertiesDownload, JSONSt
 from buildbot.steps.dummy import Dummy
 from buildbot import locks
 from buildbot.status.builder import worst_status
+from buildbot.util import json
 
 import buildbotcustom.common
 import buildbotcustom.status.errors
@@ -448,11 +449,10 @@ class MozillaBuildFactory(RequestSortingBuildFactory, MockMixin):
             property='basedir',
             workdir='.',
         ))
-        self.addStep(SetProperty(
+        self.addStep(SetBuildProperty(
             name='set_hashType',
-            command=['echo', self.hashType],
-            property='hashType',
-            workdir='.',
+            property_name='hashType',
+            value=self.hashType,
         ))
         # We need the basename of the current working dir so we can
         # ignore that dir when purging builds later.
@@ -483,6 +483,15 @@ class MozillaBuildFactory(RequestSortingBuildFactory, MockMixin):
             command=['bash', '-c', 'pwd'],
             property='toolsdir',
             workdir='tools',
+        ))
+        # Set properties for metadata about this slave
+        self.addStep(SetProperty(
+            name='set_instance_metadata',
+            extract_fn=extractJSONProperties,
+            command=['python', 'tools/buildfarm/maintenance/get_instance_metadata.py'],
+            workdir='.',
+            haltOnFailure=False,
+            warnOnFailure=False,
         ))
 
         if self.clobberURL is not None:
@@ -6369,6 +6378,15 @@ def extractProperties(rv, stdout, stderr):
         if len(e) == 2:
             props[e[0]] = e[1].strip()
     return props
+
+
+def extractJSONProperties(rv, stdout, stderr):
+    props = {}
+    try:
+        stdout = stdout.strip()
+        props.update(json.loads(stdout))
+    finally:
+        return props
 
 
 class ScriptFactory(RequestSortingBuildFactory):
