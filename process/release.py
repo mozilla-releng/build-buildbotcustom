d6e460f848c886bfb634463b83876f59d9913e2f
Mike Hommey <mh@glandium.org>
2014-06-24 07:19:55 +0900

    Bug 1027929 - Use a dedicated tooltool manifest for l10n repacks. r=nthomas

--- process/release.py
+++ process/release.py
@@ -748,8 +748,9 @@ def generateReleaseBranchObjects(releaseConfig, branchConfig,
                 if releaseConfig.get('l10nUsePymake', True) and \
                    platform in ('win32', 'win64'):
                     extra_args.append('--use-pymake')
-                if pf.get('tooltool_manifest_src'):
-                    extra_args.extend(['--tooltool-manifest', pf.get('tooltool_manifest_src')])
+                if pf.get('tooltool_l10n_manifest_src'):
+                    extra_args.extend(['--tooltool-manifest',
+                        pf.get('tooltool_l10n_manifest_src')])
                 if pf.get('tooltool_script'):
                     for script in pf['tooltool_script']:
                         extra_args.extend(['--tooltool-script', script])
@@ -834,8 +835,9 @@ def generateReleaseBranchObjects(releaseConfig, branchConfig,
                         extra_args.append('--use-pymake')
                     if releaseConfig.get('enablePartialMarsAtBuildTime', True):
                         extra_args.append('--generate-partials')
-                    if pf.get('tooltool_manifest_src'):
-                        extra_args.extend(['--tooltool-manifest', pf.get('tooltool_manifest_src')])
+                    if pf.get('tooltool_l10n_manifest_src'):
+                        extra_args.extend(['--tooltool-manifest',
+                            pf.get('tooltool_l10n_manifest_src')])
                     if pf.get('tooltool_script'):
                         for script in pf['tooltool_script']:
                             extra_args.extend(['--tooltool-script', script])
