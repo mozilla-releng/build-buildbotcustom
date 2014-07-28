fc2207fd3de7bff50a6c1146aafdf1098de2b141
Ed Morley <emorley@mozilla.com>
2014-07-28 12:26:38 +0100

    Bug 1043880 - Provide treeherder URLs alongside TBPL ones, in try push emails; r=nthomas

--- bin/try_mailer.py
+++ bin/try_mailer.py
@@ -127,6 +127,7 @@ Your %(tree)s Server %(task)s (%(revision)s) %(result_msg)s on builder %(builder
         text += "The full log for this %(task)s run is available at <a href=\"%(log_url)s\">%(log_url)s</a>.\n\n" % locals()
 
     text += "For an overview of all results see <a href=\"https://tbpl.mozilla.org/?tree=%(tree)s&rev=%(revision)s\">TBPL</a>.\n" % locals()
+    text += "Alternatively, view them on <a href=\"https://treeherder.mozilla.org/ui/#/jobs?repo=%(tree)s&revision=%(revision)s\">Treeherder</a> (experimental).\n" % locals()
     text = re.sub("\n", "<br>\n", text)
 
     headers = {"In-Reply-To": "<%(branch)s-%(revision)s>" % locals(),
