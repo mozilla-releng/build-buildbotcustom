035d571d5ef40b0f7c9b699e30f9dae4973e48a3
kmoir@mozilla.com <kmoir@mozilla.com>
2016-03-11 09:11:49 -0500

    Bug 1186617 - releng changes to decommission pandas r=aselagea

--- common.py
+++ common.py
@@ -95,7 +95,6 @@ def normalizeName(name, product=None, min_=30, max_=30, filler='0'):
         'armv6': 'a6',
         'armv7a': 'a7',
         'system': 'sys',
-        'panda': 'p',
         'b2g37': 'b37',
         'b2g44': 'b44',
         'v2_5': '25',
