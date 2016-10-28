c818247bca53b796f0e715506772f0a1ba039aba
Nick Thomas <nthomas@mozilla.com>
2016-10-28 14:41:18 +1300

    Bug 1308957 - truncate long property values to fit in statusdb column, r=catlee
    --HG--
    extra : amend_source : 0b6bea22dd8088b5803afb40fdc50ca8a153147a

--- status/db/jsoncol.py
+++ status/db/jsoncol.py
@@ -14,7 +14,7 @@ class JSONColumn(types.TypeDecorator):
     def process_bind_param(self, value, dialect):
         if value is None:
             return None
-        return json.dumps(value)
+        return json.dumps(value)[:65535]
 
     def process_result_value(self, value, dialect):
         if value is None:
